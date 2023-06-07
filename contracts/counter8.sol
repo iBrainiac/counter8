090702b3542e8616059d754d98bbb2d2bd378679
iBrainiac <franqhazard@gmail.com>
2023-06-08 00:16:24 +0300

    this is a smartcontract that adds or deducts unit8

---
+++ contracts/counter8.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter8 {
+    uint8 public counter = 0;
+
+    function increase() public {
+        counter += 8;
+    }
+
+    function decrease() public {
+        counter -= 8;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//0x8d355f87bbc4bd6d65a464124fe1374370d91c6463a0066e7af2ccbf36aa3164
