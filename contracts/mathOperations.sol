ce28890efda50fa774295c7dbd43974b62df3c6b
iBrainiac <franqhazard@gmail.com>
2023-06-08 00:34:11 +0300

    simple math operations smart contract

---
+++ contracts/mathOperations.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract mathOperations {
+    function add(uint256 y, uint256 z) public pure returns (uint256) {
+        return y + z;
+    }
+
+    function subtract(uint256 y, uint256 z) public pure returns (uint256) {
+        return y - z;
+    }
+
+    function divide(uint256 y, uint256 z) public pure returns (uint256) {
+        require(y != 0, "Cannot divide by zero");
+        return y / z;
+    }
+}
